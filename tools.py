fd260dc9f53c64e4100bdfc958387ac29ecf82da
Md Mujibur Rahman Bhuniyan <arif.mujibur@gmail.com>
2021-03-04 20:59:57 -0500

    HW1

--- tools.py
+++ tools.py
@@ -7,21 +7,32 @@ def add_intercept(X):
 
 def gradient_descent(X, Y, step_size, precision):
     w = np.zeros(X.shape[1])
-    p=1
-    while p> precision:
+    p = 1
+    while p > precision:
+        
         w = one_step( X, Y, w, step_size)
-        precision = np.abs(gradient( X, Y, w))
-    loss = loss(X@w, Y)    
-    return w, loss        
+        
+        precision = np.linalg.norm(gradient( X, Y, w))
+        #print('New W: ', w)
+    l = loss(X@w, Y)    
+    return w, l     
                  
                 
                  
 def one_step(X, Y, w, step_size):
-                 return w - step_size* gradient( X, Y, w)
+    #print('w shape: ',w.shape)
+    ans = w - step_size* gradient( X, Y, w)
+    #print('w shape: ',ans.shape)
+    return ans
     
     
 def gradient(X, Y, w):
-    return -2*(Y - X@w)@ X
+    """print('X shape: ',X.shape)
+    print('Y shape: ',Y.shape)
+    print('w shape: ',w.shape)"""
+    ans = -2.*(Y - X@w)@ X
+    #print(ans.shape)
+    return ans
 
 def loss( Y_hat, Y):
     return (Y-Y_hat)@(Y-Y_hat)
